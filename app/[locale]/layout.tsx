970cf2f4a70ed7add047ee4fcd665b1eef8e6007
JohnIsHereash <diminishtime@gmail.com>
2025-06-19 14:44:11 +0800

    Update layout.tsx

--- app/[locale]/layout.tsx
+++ app/[locale]/layout.tsx
@@ -97,7 +97,6 @@ export default async function RootLayout({
           >
             <Toaster richColors position="top-center" duration={3000} />
             <div className="bg-background text-foreground flex h-dvh flex-col items-center overflow-x-auto">
-              {session ? <GlobalState>{children}</GlobalState> : children}
             </div>
           </TranslationsProvider>
         </Providers>
